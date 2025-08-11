f250eaa2e393ed85d4cb80e9f2b4a5f5b8aaba3a
Hayk10002 <hayk.g.khachatryan@gmail.com>
2025-08-11 15:27:49 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# ModularityVSEfficiency
+
+## Table of Contents
+- [Introduction](#introduction)
+- [Build and Run](#build-and-run)
+- [Possible Output](#possible-output)
+- [How does this work](#how-does-this-work)
+
+The purpose of this project is to explore the trade-off between modularity and efficiency when processing large vectors. By comparing separate, modular loops with a fused, more efficient loop, the code demonstrates how increasing modularity can sometimes come at the cost of performance, particularly in terms of cache usage and execution time.
+
+## Build and Run
+To clone and run this project, you'll need [Git](https://git-scm.com) and [CMake](https://cmake.org/) installed on your computer. From your command line:
+
+```bash
+# Clone this repository
+$ git clone https://github.com/Hayk10002/ModularityVSEfficiency
+
+# Go into the repository
+$ cd ModularityVSEfficiency
+
+# We need to generate the executable in Release configuration to be able to observe the time difference between fused and non-fused loops
+
+# Generate the build files
+$ cmake -DCMAKE_BUILD_TYPE=Release -S . -B build
+
+# Build the project
+$ cmake --build build --config Release
+
+# Then, run the executable generated in the `build` directory with two number arguments.
+$ your/path/to/exe/main.exe {vector_size} {iteration_count}
+# example - .../main.exe 10000 1000
+```
+
+## Possible Output
+
+```
+Time not fused:   240ms
+Time     fused:    68ms
+```
+
+## How does this work
+The program initializes three large vectors with random integers. It then measures the time taken to compute the result vector in two ways:
+- **Not fused:** Performs multiplication and addition in separate loops (using separate functions).
+- **Fused:** Performs both operations in a single loop (using a fused function).
+
+By timing both approaches over multiple iterations, the program demonstrates the effect of loop fusion on cache efficiency and overall performance. Fusing loops can reduce memory traffic and improve cache locality, often resulting in faster execution times.
+
+Note: The actual timing results will depend on your hardware and the input sizes you provide.
---
+++ main.cpp
@@ -0,0 +1,97 @@
+    #include <iostream>
+    #include <vector>
+    #include <list>
+    #include <chrono>
+    #include <random>
+    #include <format>
+    #include <cassert>
+    #include <algorithm>
+
+
+    #if defined(_MSC_VER)      // Microsoft Visual C++
+    #define NO_INLINE __declspec(noinline)
+    #elif defined(__GNUC__) || defined(__clang__) // GCC or Clang
+    #define NO_INLINE __attribute__((noinline))
+    #elif defined(__INTEL_COMPILER) // Intel C/C++
+    #define NO_INLINE __attribute__((noinline))
+    #else
+    #define NO_INLINE /* noinline not supported */
+    #endif
+
+    NO_INLINE std::vector<int> add(const std::vector<int>& a, const std::vector<int>& b)
+    {
+        std::vector<int> res(a.size());
+        for (int i = 0; i < a.size(); i++) res[i] = a[i] + b[i];
+        return res;
+    }
+
+    NO_INLINE std::vector<int> mult(const std::vector<int>& a, const std::vector<int>& b)
+    {
+        std::vector<int> res(a.size());
+        for (int i = 0; i < a.size(); i++) res[i] = a[i] * b[i];
+        return res;
+    }
+
+    NO_INLINE std::vector<int> mult_add(const std::vector<int>& a, const std::vector<int>& b, const std::vector<int>& c)
+    {
+        std::vector<int> res(a.size());
+        for (int i = 0; i < a.size(); i++) res[i] = a[i] * b[i] + c[i];
+        return res;
+    }
+
+    // Random number from 0 to max-1 inclusive
+    int getRandomNum(int max)
+    {
+        static std::mt19937 engine{ std::random_device{}() };
+        std::uniform_int_distribution<int> dist(0, max - 1);
+        return dist(engine);
+    }
+
+    template<class F>
+    auto time_ns(F f, int N)
+    {
+        auto start = std::chrono::high_resolution_clock::now();
+
+        for (int i = 0; i < N; i++) f();
+
+        return std::chrono::high_resolution_clock::now() - start; 
+    }
+
+    int main(int argc, char* argv[]) {
+        if (argc != 3) { throw std::runtime_error(std::format("Invalid command line arguments. Correct usage: {} <vector_size> <iteration_count>", argv[0])); }
+        int vec_size = std::stoi(argv[1]);
+        int iter_count = std::stoi(argv[2]); 
+        std::vector<int> first(vec_size), second(vec_size), third(vec_size);
+        std::vector<int> res;
+        for(int i = 0; i < vec_size; i++) first [i] = getRandomNum(1000);
+        for(int i = 0; i < vec_size; i++) second[i] = getRandomNum(1000);
+        for(int i = 0; i < vec_size; i++) third[i] = getRandomNum(1000);
+
+        volatile int x;
+
+        auto separate_time = time_ns([&]()
+            {
+                res = add(mult(first, second), third);
+                x += res[getRandomNum(res.size() - 1)];
+            },
+            iter_count
+        );
+
+        auto fused_time = time_ns([&]()
+            { 
+                res = mult_add(first, second, third);
+                x += res[getRandomNum(res.size() - 1)];
+            },
+            iter_count
+        );
+        
+        
+        std::cout << std::format(
+            "Time not fused: {:>7}\n"
+            "Time     fused: {:>7}\n",
+            std::chrono::duration_cast<std::chrono::milliseconds>(separate_time),
+            std::chrono::duration_cast<std::chrono::milliseconds>(fused_time)
+        );
+
+        return 0;
+    }
